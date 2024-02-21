cd279d5c8792bc9c19917d32ed76d51942afb912
Tikiid <thibo92s@gmail.com>
2024-02-21 17:09:50 +0100

    Add batiment class

---
+++ Batiment.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+class Batiment:
+    def __init__(self, id_building, list_infras):
+        self.id_building = id_building
+        self.list_infras = list_infras
+
+    def get_building_difficulty(self):
+        pass
