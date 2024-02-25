e2bf72b2e5839bcf004e1ed780ffff7e2faf37ee
Tikiid <thibo92s@gmail.com>
2024-02-25 20:30:50 +0100

    Add get_building_difficulty in batiment class

--- Batiment.py
+++ Batiment.py
@@ -4,6 +4,10 @@ class Batiment:
     def __init__(self, id_building, list_infras):
         self.id_building = id_building
         self.list_infras = list_infras
+        
 
     def get_building_difficulty(self):
-        pass
+        bat_difficulty = 0
+        for element in self.list_infras:
+            bat_difficulty += element.get_infra_difficulty()
+        return bat_difficulty
