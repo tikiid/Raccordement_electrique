f4389957f35fbd92cc962775acc559cae027d431
Tikiid <thibo92s@gmail.com>
2024-02-25 20:31:17 +0100

    Change get_infra_difficulty in infra Class

--- Infrastructure.py
+++ Infrastructure.py
@@ -6,11 +6,12 @@ class Infra:
         self.length = infra_length
         self.infra_type = infra_type
         self.nb_houses =  nb_houses
-        self.infra_difficulty = 0.0
+        
+        
         
     def repair_infra(self):
         if self.infra_type == "a_remplacer": 
             self.infra_type = "infra_intacte"
     
     def get_infra_difficulty(self):
-        self.infra_difficulty = self.length / self.nb_houses
+        return self.length / self.nb_houses
